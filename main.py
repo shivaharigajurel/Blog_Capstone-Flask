8bc3b011d4bc78b78eb7959336ff53fff4f18ecb
shivaharigaj <shivaharigaj@gmail.com>
2024-10-19 14:10:55 +0545

    Your commit message here

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-
+OWN_EMAIL = 'YOUR OWN EMAIL ADDRESS'
+OWN_PASSWORD = 'YOUR EMAIL ADDRESS PASSWORD'
 app = Flask(__name__)
 
 
@@ -17,10 +19,21 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods = ["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 @app.route("/post/<int:index>")
 def show_post(index):
@@ -32,4 +45,4 @@ def show_post(index):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
